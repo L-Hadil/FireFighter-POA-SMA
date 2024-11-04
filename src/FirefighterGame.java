88844db22b8bef76ef23be81dfb810d20081488f
MANSOUR Malik <67902643+malik-mnsr@users.noreply.github.com>
2024-11-04 15:33:42 +0100

    Adding a synchronisation mechanism

--- src/FirefighterGame.java
+++ src/FirefighterGame.java
@@ -1,7 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class FirefighterGame extends JPanel {
     private static final int GRID_SIZE = 10;
@@ -13,12 +16,20 @@ public class FirefighterGame extends JPanel {
     private boolean gameOver = false;
     private String winnerMessage = "";
 
+    private final Image fireIcon;
+    private final Image firefighterIcon;
+    private boolean fireAgentTurn = true; // Boolean to alternate turns
+
     public FirefighterGame() {
         setPreferredSize(new Dimension(GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE + 50));
         grid = new Grid(GRID_SIZE, OBJECTIVES_COUNT);
 
         fireAgent = new FireAgent((int) (Math.random() * GRID_SIZE), (int) (Math.random() * GRID_SIZE), grid, OBJECTIVES_COUNT);
         firefighterAgent = new FirefighterAgent(GRID_SIZE / 2, GRID_SIZE / 2, grid, OBJECTIVES_COUNT);
+
+        // Load icons
+        fireIcon = new ImageIcon("images/img_3.png").getImage().getScaledInstance(CELL_SIZE, CELL_SIZE, Image.SCALE_SMOOTH);
+        firefighterIcon = new ImageIcon("images/img_1.png").getImage().getScaledInstance(CELL_SIZE, CELL_SIZE, Image.SCALE_SMOOTH);
     }
 
     @Override
@@ -36,13 +47,13 @@ public class FirefighterGame extends JPanel {
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
                 if (grid.isFireAt(i, j)) {
-                    g.setColor(new Color(255, 150, 150)); // Rouge clair pour les cases en feu
+                    g.setColor(new Color(255, 150, 150)); // Light red for burning cells
                 } else if (grid.isSafeAt(i, j)) {
-                    g.setColor(Color.CYAN); // Cyan pour les cases sécurisées
+                    g.setColor(Color.CYAN); // Cyan for safe cells
                 } else if (grid.isBarrierAt(i, j)) {
-                    g.setColor(Color.GRAY); // Gris pour les barrières
+                    g.setColor(Color.GRAY); // Gray for barriers
                 } else {
-                    g.setColor(Color.WHITE); // Blanc pour les cellules vides
+                    g.setColor(Color.WHITE); // White for empty cells
                 }
                 g.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                 g.setColor(Color.BLACK);
@@ -51,21 +62,22 @@ public class FirefighterGame extends JPanel {
         }
     }
 
-
     private void drawFirefighter(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillOval(firefighterAgent.getX() * CELL_SIZE, firefighterAgent.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        g.drawImage(firefighterIcon, firefighterAgent.getX() * CELL_SIZE, firefighterAgent.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE, this);
     }
 
     private void drawFire(Graphics g) {
-        g.setColor(Color.RED);
-        g.fillOval(fireAgent.getX() * CELL_SIZE, fireAgent.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        g.drawImage(fireIcon, fireAgent.getX() * CELL_SIZE, fireAgent.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE, this);
     }
 
     private void drawObjectives(Graphics g) {
-        g.setColor(Color.GREEN);
-        for (int[] obj : grid.getObjectives()) {
-            g.fillOval(obj[0] * CELL_SIZE, obj[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        try {
+            Image greenIcon = ImageIO.read(new File("images/img_4.png"));
+            for (int[] obj : grid.getObjectives()) {
+                g.drawImage(greenIcon, obj[0] * CELL_SIZE, obj[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE, this);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -81,7 +93,7 @@ public class FirefighterGame extends JPanel {
     }
 
     private void checkGameOver() {
-        if (!grid.hasObjectives()) { // Vérification si tous les objectifs sont atteints ou brûlés
+        if (!grid.hasObjectives()) {
             gameOver = true;
             int firefighterScore = firefighterAgent.getScore();
             int fireScore = fireAgent.getScore();
@@ -94,13 +106,9 @@ public class FirefighterGame extends JPanel {
                 winnerMessage = "Match nul ! Les scores sont égaux : " + firefighterScore;
             }
 
-            System.out.println(winnerMessage);
-
-            // Afficher une boîte de dialogue avec le message du gagnant
             JOptionPane.showMessageDialog(this, winnerMessage, "Résultat de la Partie", JOptionPane.INFORMATION_MESSAGE);
         }
     }
-
 
     public void playGame() {
         JFrame frame = new JFrame("Firefighter Game");
@@ -110,22 +118,21 @@ public class FirefighterGame extends JPanel {
         frame.setVisible(true);
 
         Timer timer = new Timer(1000, new ActionListener() {
-            int turn = 1;
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (!gameOver) {
-                    System.out.println("== Tour " + turn + " ==");
-
-                    firefighterAgent.move();
-                    System.out.println("Pompier : Score actuel = " + firefighterAgent.getScore());
-
-                    fireAgent.move();
-                    System.out.println("Feu : Score actuel = " + fireAgent.getScore());
-
+                    if (fireAgentTurn) {
+                        fireAgent.move();
+                        System.out.println("--------------Fire Agent Turn");
+                        System.out.println("Feu : Score actuel = " + fireAgent.getScore());
+                    } else {
+                        System.out.println("//////////////Fire Fighter Agent Turn");
+                        firefighterAgent.move();
+                        System.out.println("Pompier : Score actuel = " + firefighterAgent.getScore());
+                    }
                     checkGameOver();
                     repaint();
-                    turn++;
+                    fireAgentTurn = !fireAgentTurn;
                 }
             }
         });
